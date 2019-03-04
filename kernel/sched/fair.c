5540bf8d766dee252eaddadb285b70aae63e0f28
yncxcw <ynjassionchen@gmail.com>
2019-03-04 16:26:40 -0700

    add_trace_load_balance

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8482,8 +8482,10 @@ static int should_we_balance(struct lb_env *env)
 
 
 
-static void trace_load_balance(int src_cpu, int dest_cpu, int num_task){
+static void trace_load_balance(int src_cpu, int des_cpu, int num_task){
      
+    
+    printk("%d   %d   %d", src_cpu, des_cpu, num_task);
     return;
 }
 
