4c7fe7edb998e0f86da04676a708d5590df80039
yncxcw <ynjassionchen@gmail.com>
2019-03-04 17:57:35 -0700

    remoe printk

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8483,9 +8483,6 @@ static int should_we_balance(struct lb_env *env)
 
 
 static int __attribute__((optimize("O0"))) trace_load_balance(int src_cpu, int des_cpu, int num_task){
-   
-    //avoid to be optimized by compiler
-    printk("%d %d %d", src_cpu, des_cpu, num_task);
        
     return num_task;
 }
