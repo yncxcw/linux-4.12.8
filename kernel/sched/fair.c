6a728c6bfbbf38b4a8d0c97db0d8ccd315497c33
yncxcw <ynjassionchen@gmail.com>
2019-03-07 17:31:48 -0700

    printk load balance interval

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8332,7 +8332,8 @@ force_balance:
 	/* Looks like there is an imbalance. Compute it */
 	calculate_imbalance(env, &sds);
         //print load related information
-        if(sds.busiest && sds.local){
+        if(0){    
+        //if(sds.busiest && sds.local){
             unsigned long local_cpu = cpumask_bits(sched_group_span(sds.local))[0];
             printk("local group: avgload %lu groupload %lu grouputil %lu groupcapacity %lu nr_tasks %d cpu %lu", 
                     sds.local_stat.avg_load,
@@ -8764,6 +8765,8 @@ more_balance:
 		if (sd->balance_interval < sd->max_interval)
 			sd->balance_interval *= 2;
 	}
+        
+        printk("%lu %lu %lu", sd->balance_interval, sd->min_interval, sd->max_interval);
 
 	goto out;
 
